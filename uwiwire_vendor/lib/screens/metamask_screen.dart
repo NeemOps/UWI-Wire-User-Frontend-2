a743339528b90ac78c5e3fc779b398dd30a13f3e
Bradley <bradleyjesseb@gmail.com>
2023-04-09 12:45:47 -0300

    connects to metamask

--- uwiwire_vendor/lib/screens/metamask_screen.dart
+++ uwiwire_vendor/lib/screens/metamask_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -47,16 +48,20 @@ class _MetamaskScreenState extends State<MetamaskScreen> {
       }),
     );
 
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => connectMetamask(context),
-              child: const Text("Connect Metamask"),
-            )
-          ],
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              (_session != null)
+                  ? WalletInfo(session: _session)
+                  : ElevatedButton(
+                      onPressed: () => connectMetamask(context),
+                      child: const Text("Connect Metamask"),
+                    )
+            ],
+          ),
         ),
       ),
     );
@@ -69,8 +74,7 @@ class _MetamaskScreenState extends State<MetamaskScreen> {
           _uri = uri;
           await launchUrlString(uri, mode: LaunchMode.externalApplication);
         });
-        print(session.accounts[0]);
-        print(session.chainId);
+
         setState(() {
           _session = session;
         });
@@ -78,5 +82,36 @@ class _MetamaskScreenState extends State<MetamaskScreen> {
         print(exp);
       }
     }
+  }
+}
+
+class WalletInfo extends StatelessWidget {
+  const WalletInfo({
+    super.key,
+    required session,
+  }) : _session = session;
+
+  final _session;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Account',
+              style:
+                  GoogleFonts.ubuntu(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+
+            // Display Account Address
+            Text(
+              '${_session.accounts[0]}',
+              style: GoogleFonts.inconsolata(fontSize: 16),
+            ),
+          ],
+        ));
   }
 }
